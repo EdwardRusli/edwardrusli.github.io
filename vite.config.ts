dbb784913e48af038797a7f50de899f68b4ce3fc
Edward Rusli <43808831+EdwardRusli@users.noreply.github.com>
2025-07-07 19:48:37 +0700

    react initial

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
-});
+  base: "/edwardrusli.github.io/",
+  plugins: [react()],
+})
